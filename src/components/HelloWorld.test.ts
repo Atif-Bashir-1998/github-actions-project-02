97a48de54248f3107bfb6c2825d529841a29a707
Atif-Bashir-1998 <adobe8832289@gmail.com>
2025-09-13 20:48:07 +0300

    added very basic setup for tests

---
+++ src/components/HelloWorld.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from 'vitest';
+
+describe('HelloWorld', () => {
+  // Test 1: Checks if the component renders the message prop correctly.
+  test('sanity check', () => {
+
+    expect(2+2).toBe(4);
+  });
+});
