8792885e5566e346caf8036770dc93abc6ca8be5
Atif-Bashir-1998 <adobe8832289@gmail.com>
2025-09-13 20:55:41 +0300

    failing test on purpose

--- src/components/HelloWorld.test.ts
+++ src/components/HelloWorld.test.ts
@@ -4,6 +4,6 @@ describe('HelloWorld', () => {
   // Test 1: Checks if the component renders the message prop correctly.
   test('sanity check', () => {
 
-    expect(2+2).toBe(4);
+    expect(2+3).toBe(4);
   });
 });
